c4f5667b9ad48a5916c1a0f83539ae3e0dd677e3
Jarrod Colwell <jarrodcolwell@gmail.com>
2020-01-26 14:37:53 -0500

    Added files without extras

---
+++ Questions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questions : MonoBehaviour
+{
+    private int num = 1;
+    public AudioSource clip1;
+    public AudioSource clip2;
+    public AudioSource clip3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (num == 1)
+            {
+                clip2.Play();
+                num++;
+            }
+            else if (num ==2)
+            {
+                clip3.Play();
+                num++;
+            }
+        }
+    }
+}
---
+++ SceneChange.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public int num;
+    private int counter = 1;
+    public AudioSource audioSource;
+    public AudioClip clip2;
+    public AudioClip clip3;
+
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (num == 3 && counter < 3)
+            {
+                if (counter == 1)
+                    audioSource.PlayOneShot(clip2);
+                else if (counter == 2)
+                    audioSource.PlayOneShot(clip3);
+                counter++;
+            }
+            else
+            {
+                SceneManager.LoadScene(num);
+                Debug.Log("Scene changed to " + num);
+            }
+        }
+    }
+}
